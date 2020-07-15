113cb34b7ff351fc3f50c167da157b227f270fda
Yuriy Panasiuk <yur.panasiuk@gmail.com>
2020-07-15 11:15:48 +0300

    Updates

---
+++ scripts/main.js
@@ -0,0 +1,20 @@
+'use strict';
+/* eslint-disable no-undef */
+
+AOS.init({
+  disable: false,
+  startEvent: 'DOMContentLoaded',
+  animatedClassName: 'aos-animate',
+  useClassNames: true,
+  disableMutationObserver: false,
+  debounceDelay: 50,
+  throttleDelay: 99,
+
+  offset: 120,
+  delay: 0,
+  duration: 400,
+  easing: 'ease',
+  once: false,
+  mirror: false,
+  anchorPlacement: 'top-bottom',
+});
